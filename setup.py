acf91eb05f555e15d0e3d9b70bb2f8970f9137f0
Andrei Lapets <lapets@gmail.com>
2015-11-25 22:41:07 -0500

    Update contact email address.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     license          = 'MIT License',
 	url              = 'http://pydrogen.org',
 	author           = 'A. Lapets',
-	author_email     = 'lapets@gmail.com',
+	author_email     = 'a@lapets.io',
     description = 'Python library for building embedded languages within Python that have alternative operational semantics and abstract interpretations..',
     long_description = open('README.md').read(),
 )
