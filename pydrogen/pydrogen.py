34b28704d1cc854744ccb0add138606ce0063a1f
Andrei Lapets <lapets@gmail.com>
2016-01-01 23:54:16 -0500

    Initial class definition.

--- pydrogen/pydrogen.py
+++ pydrogen/pydrogen.py
@@ -17,9 +17,23 @@
 
 import ast     # For Python abstract syntax trees.
 import inspect # To retrieve a function body source.
-import sympy   # For symbolic polynomials and other expressions.
+#import sympy   # For symbolic polynomials and other expressions.
+
+# The Pydrogen class can be extended to define a new operational
+# semantics or abstract interpretation for abstract syntax trees,
+# and then used as a decorator that is applied to functions that
+# must be processed using that alternative semantics/interpretation.
+class Pydrogen():
+    def Num():
+        return None
+
+    def Str():
+        return None
+
+    def Bytes():
+        return None
 
 def __function(name):
-    return ast.parse(inspect.getsource(name))
+    return ast.parse(inspect.getsource(name))   
 
 ##eof
