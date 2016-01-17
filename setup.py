e9409888e86a7fc52786dc08a1a6906f1b267cf9
Andrei Lapets <lapets@gmail.com>
2016-01-17 18:16:47 -0500

    Update webpage content, README, dependencies, and version number.

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 
 setup(
     name             = 'pydrogen',
-    version          = '0.0.1.0',
+    version          = '0.0.2.0',
     packages         = ['pydrogen',],
+    install_requires = ['ast', 'inspect',],
     license          = 'MIT License',
 	url              = 'http://pydrogen.org',
 	author           = 'A. Lapets',
 	author_email     = 'a@lapets.io',
     description = 'Python library for building embedded languages within Python that have alternative operational semantics and abstract interpretations..',
-    long_description = open('README.md').read(),
+    long_description = open('README').read(),
 )
