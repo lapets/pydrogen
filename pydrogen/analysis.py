d5a37385666dd7ca690020b78541a8d585f7ab6f
Tim Lim <timlim99@hotmail.com>
2016-12-02 14:54:23 -0500

    repackaging into analysis module

--- pydrogen/analysis.py
+++ pydrogen/analysis.py
@@ -4,19 +4,26 @@ import pydrogen
 import sympy
 from sympy import O, symbols, oo
 
+
+# some complexity classes using sympy's O notation
+linear = lambda n: O(n, (n, oo))
+quadratic = lambda n: O(n**2, (n,oo))
+logarithmic = lambda n: O(sympy.log(n, 2), (n,oo))
+exponential = lambda n: O(2**n, (n,oo))
+constant = lambda _: O(1)
+
 class Complexity(pydrogen.Typical):
     """ Complexity approximation for a small subset of Python. """
-    # some complexity classes using sympy's O notation
-    poly = lambda _, n, power: O(n**power, (n, oo))
-    log = lambda _, n: O(sympy.log(n), (n,oo))
-    exp = lambda _, n, base: O(base**n, (n,oo))
-    constant = O(1)
 
     # store the complexity of some known functions for added expressibility
     functions = {'len': constant}
 
     def preprocess(self, context):
         syms = []
+        # allow for specification of running time of certain functions as well (?)
+        if 'functions' in context and type(context['functions']) == dict:
+            self.functions.update(context['functions'])
+            del context['functions']
         for var, symbol in context.items():
             if type(symbol) == str:
                 symbol = symbols(symbol)
@@ -24,43 +31,71 @@ class Complexity(pydrogen.Typical):
             syms.append((symbol, oo))
 
     def Statements(self, ss, context=None): return sum(ss.post(context)[0])
-    def Assign(self, targets, e, context=None): return self.constant + e.post(context)
+    def Assign(self, targets, e, context=None): return constant(None) + e.post(context)
     # if iterable maps to a symbol, use it. otherwise, assume it is constant.
     def For(self, target, itr, ss, orelse, context=None):
-        itr = itr.pre().id
-        if itr in context:
-            loops = self.poly(context[itr], 1)
+        # we make the simplifying assumption that whatever being iterated over
+        # is either:
+        #   a named variable, where we look up the context for a symbol and
+        #       regard as constant size if not present
+        #   a built-in sequence (list, string, dictionary, tuple, set) which we
+        #       regard as constant size
+        #   a function call, where we assume that the complexity of the function
+        #       call is also the size of the iterable returned. This is
+        #       obviously not always the case but we assume relatively
+        #       simple functions.
+        if hasattr(itr.pre(), 'id'):
+            itr = itr.pre().id
+            if itr in context:
+                loops = linear(context[itr])
+            else:
+                loops = constant(None)
+        elif type(itr.pre()) in (ast.List, ast.Tuple, ast.Str, ast.Dict, ast.Set):
+            loops = constant(None)
         else:
-            loops = self.constant
+            loops = itr.post(context)
         work = ss.post(context)
         # sympy raises error if you try to do O(1) * O(x, (x,oo)) for e.g.
         return loops * work if work.contains(loops) else work * loops
-    def BoolOp(self, es, context=None): return self.constant + sum(es.post(context))
-    def BinOp(self, e1, e2, context): return self.constant + e1.post(context) + e2.post(context)
+    def BoolOp(self, es, context=None): return constant(None) + sum(es.post(context))
+    def BinOp(self, e1, e2, context): return constant(None) + e1.post(context) + e2.post(context)
     def Call(self, func, args, context=None):
         func = func.pre()
         if context is None:
             context = {}
-        return self.functions[func.id] if func.id in self.functions else Complexity(func, **context)
+        if func.id in self.functions:
+            symbol = None
+            # check context if any arg maps to a symbol; take first one we find
+            # and return complexity class of the function.
+            for arg in args.pre():
+                if arg.id in context:
+                    symbol = context[arg.id]
+                    break
+            if symbol or self.functions[func.id] == constant:
+                return self.functions[func.id](symbol)
+        # otherwise, attempt to interpret the function directly
+        return Complexity(func, **context)
     def Num(self, n, context=None): return 0
     def NameConstant(self): return 0
     def Name(self, id, context=None): return 0
+    def If(self, test, body, orelse, context=None):
+        return test.post(context) + body.post(context) + orelse.post(context)
+    def Compare(self, l, r, context=None):
+        return constant(None) + l.post(context) + r.post(context)
 
 @Complexity(items='n')
 def average(items):
     total = 0
-    for item in items:
+    for item in [1,2,3,4]:
         for item in items:
             total = total + item
     return total/len(items)
 
+# print('The asymptotic complexity of average() is ' + str(average.Complexity) + '.')
 
-#####################################################################
-## Defining a running time approximation algorithm for a small subset
-## of Python.
-##
 
 class Size(pydrogen.Typical):
+    """ Analyze size of AST for a subset of Python. """
     def Statements(self, ss): return sum(ss.post())
     def List(self, elts): return len(elts.post())
     def Num(self, n): return n.post()
@@ -69,6 +104,7 @@ class Size(pydrogen.Typical):
     def Name(self): return 1
 
 class Time(pydrogen.Typical):
+    """ Numeric running time approximation for a small subset of Python. """
     def Statements(self, ss): return sum(ss.post())
     def Assign(self, targets, e): return 1 + e.post()
     def For(self, target, itr, ss, orelse): return Size(itr.pre()) * ss.post()
@@ -103,13 +139,6 @@ def example():
     for i in range(100):
         print(i + i + 1)
 
+# print('The approximate running time of example() is ' + str(example.Time) + '.')
 
-print("The approximate running time of example() is " + str(example.Time) + ".")
-
-
-"""
-@Time
-def eg():
-    return [a*y for a in [1,2,3,4] if a == 1 for y in [1,2,3,4] if y == 2]
-"""
 ##eof
